919e2cf55114d3878c75a25c13838fe6ca56fc39
rosswired <rwoodhurst@gmail.com>
2021-02-18 22:35:18 +0000

    changed path = string

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -15,7 +15,7 @@ export async function getStaticProps({ params }) {
   const userDoc = await getUserWithUsername(username);
 
   let post = null;
-  let path = null;
+  let path = "";
 
   if (userDoc) {
     const postRef = userDoc.ref.collection('posts').doc(slug);
